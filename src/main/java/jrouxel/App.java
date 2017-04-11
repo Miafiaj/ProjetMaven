e7e160d38ab3bba8f1c3e8f57657694336741c82
Rouxel Jeremy <jrouxel@SA203-07A.ensi-bourges.fr>
2017-04-11 18:41:29 +0200

    suppr

--- src/main/java/jrouxel/App.java
+++ src/main/java/jrouxel/App.java
@@ -1,4 +1,12 @@
 package jrouxel;
+
+import java.util.*;
+import java.io.*;
+import java.awt.List;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import com.opencsv.CSVReader;
+
 /**
  * Hello world!
  *
@@ -7,13 +15,38 @@ public class App
 {
     public static int max(int a, int b)
     {
-    	//return a > b ? a:b;
-    	return a;
+    	return a > b ? a:b;
+    	//return a;
     }
     
     public static void main( String[] args )
     {
-    	int result = max(4,5);
-        System.out.println( "Max :" + result );
+    	//int result = max(4,5);
+        //System.out.println( "Max :" + result );
+    	int monmax=0;
+    	String nb;
+        int temp;
+    	try 
+    	{
+			CSVReader reader = new CSVReader(new FileReader("data.csv"),',');
+			List<String[]> myEntries = reader.readAll();
+			String[] test;
+	       	//parcourt
+	       	for (int i  = 0; i<myEntries.size(); i++)
+	       	{
+	       		test=myEntries.get(i);
+	       		nb=test[0];
+	       		temp=Integer.parseInt(nb);
+	       		if (max(monmax,temp)!=monmax)
+	       		{
+	       			monmax=temp;
+	       		}
+	       	}
+	       	System.out.println("Max :" + monmax);
+    	} catch (FileNotFoundException e) 
+    		{
+				e.printStackTrace();
+			}
+    	
     }
 }
